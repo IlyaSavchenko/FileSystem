01b6d9b9805be74ddf5a873dbdf3fb8532e954ae
IlyaSavchenko <sav.il.yu@gmail.com>
2013-12-21 22:08:56 +0400

    add constructiontree

--- FileSystem.c
+++ FileSystem.c
@@ -1,12 +1,15 @@
-#include stdlib.h>
-#include fuse.h>
-#include stdio.h>
-#include string.h>
+#include <stdlib.h>
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
 
 typedef struct Node* nodeRef; 
 typedef struct Node{
 	cahr* nodeName;
 	char* nodeData;
+	nodeRef* nodeParent;
+	nodeRef nodeChild*;
+	int countOfChild;
  } node;
 
  nodeRef treeSystem;
@@ -17,4 +20,15 @@ int main(int argc, char const *argv[])
 {
 	/* code */
 	return 0;
+}
+
+nodeRef Construction()
+{
+    nodeRef treeSystem = (nodeRef)malloc(sizeof(node));
+    treeSystem->nodeName = ""; 
+    treeSystem->nodeData = 0;
+    treeSystem->nodeChild = 0;
+    treeSystem->nodeParent = 0;
+    treeSystem->countOfChild = 0;
+    return treeSystem;
 }

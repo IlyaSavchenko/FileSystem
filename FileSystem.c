f96048af0a99e159e13d5b2cb7880e37a68221cb
IlyaSavchenko <sav.il.yu@gmail.com>
2013-12-21 20:53:31 +0400

    added struct for tree

--- FileSystem.c
+++ FileSystem.c
@@ -2,3 +2,19 @@
 #include fuse.h>
 #include stdio.h>
 #include string.h>
+
+typedef struct Node* nodeRef; 
+typedef struct Node{
+	cahr* nodeName;
+	char* nodeData;
+ } node;
+
+ nodeRef treeSystem;
+ nodeRef* nowNode;
+ nodeRef* nowNodeData = "";
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
